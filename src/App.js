650e2971b37b4617724ff7968609d3b5be006c9b
Anas Qazi <anas@capstack.ca>
2021-04-16 01:25:45 -0400

    installed thee giphy packages along with test from docs - consoling values

--- src/App.js
+++ src/App.js
@@ -1,16 +1,42 @@
 
-import { DatePicker } from 'antd';
-
 import { Input, Space } from 'antd';
-import { AudioOutlined } from '@ant-design/icons';
+import { GifOutlined } from '@ant-design/icons';
+
+import { Grid } from '@giphy/react-components'
+import { GiphyFetch } from '@giphy/js-fetch-api'
 
 import './App.css';
 import 'antd/dist/antd.css';
 
+const gf = new GiphyFetch('BwLH6EekaraNN4YwsuWCmaVrKkbrrPHz');
+
+
+const trending = async () => {
+  try {
+    const result = await gf.trending({ limit: 10, offset: 25, rating: 'g' });
+    console.log(`trending`, result);
+  } catch (error) {
+    console.error(`trending`, error);
+  }
+};
+
+const searchGifs = async () => {
+  try {
+    const result = await gf.search("dogs", { limit: 10, sort: "recent" });
+    console.log(`search`, result);
+  } catch (error) {
+    console.error(`search`, error);
+  }
+};
+
+
+searchGifs();
+trending();
+
 const { Search } = Input;
 
 const suffix = (
-  <AudioOutlined
+  <GifOutlined
     style={{
       fontSize: 16,
       color: '#1890ff',
@@ -18,7 +44,9 @@ const suffix = (
   />
 );
 
-const onSearch = value => console.log(value);
+const onSearch = (value) => {
+  console.log(value)
+};
 
 
 function App() {
@@ -31,6 +59,12 @@ function App() {
         suffix={suffix}
         onSearch={onSearch}
         allowClear
+      />
+      <Space />
+      <Grid 
+      width={800} 
+      columns={5} 
+      // fetchGifs={trending} 
       />
     </div>
   );
