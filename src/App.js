6354071dbd83509746a4b1104d5c99819a80fc00
Anas Qazi <anas@capstack.ca>
2021-04-16 00:55:15 -0400

    added search input field from ant design

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
 
 import { DatePicker } from 'antd';
+
+import { Input, Space } from 'antd';
+import { AudioOutlined } from '@ant-design/icons';
+
 import './App.css';
 import 'antd/dist/antd.css';
+
+const { Search } = Input;
+
+const suffix = (
+  <AudioOutlined
+    style={{
+      fontSize: 16,
+      color: '#1890ff',
+    }}
+  />
+);
+
+const onSearch = value => console.log(value);
+
 
 function App() {
   return (
     <div className="App">
-      <DatePicker />
+      <Search
+        placeholder="input search text"
+        enterButton="Search"
+        size="large"
+        suffix={suffix}
+        onSearch={onSearch}
+        allowClear
+      />
     </div>
   );
 }
