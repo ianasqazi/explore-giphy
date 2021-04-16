fafdd68933df329c60fa2ef0ca0a9f95f0a16c30
Anas Qazi <anas@capstack.ca>
2021-04-16 06:20:35 -0400

    updated styling and fixes

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import { GiphyFetch } from '@giphy/js-fetch-api';
 import ResultList from './components/ResultList';
 import axios from "axios";
 import 'antd/dist/antd.css';
+import './App.css'
 
 const TRENDINGURL = "https://api.giphy.com/v1/gifs/trending"
 
@@ -73,85 +74,79 @@ function App() {
       <Row>
         <Col span={24}  >
 
-        <Tabs defaultActiveKey="1" centered>
+          <Tabs defaultActiveKey="1" centered>
 
-          <TabPane tab="Using CSS Library - Ant Design" key="1">
-            <Row align={'center'} gutter={20}>
+            <TabPane tab="Using CSS Library - Ant Design" key="1">
+              <Row align={'center'} gutter={20}>
 
-              <Col span={24} align='center'
-                style={{ marginTop: '30px' }}>
-                <h3>Top trending GIPHYS </h3>
-              </Col>
+                <Col span={24} align='center'
+                  style={{ marginTop: '30px' }}>
+                  <h3>Top trending GIPHYS </h3>
+                </Col>
 
-              <Col span={20}>
-                <ResultList results={ourGiphys} />
-              </Col>
-            </Row>
-          </TabPane>
+                <Col span={20}>
+                  <ResultList results={ourGiphys} />
+                </Col>
+              </Row>
+            </TabPane>
 
-          <TabPane tab="Using fetchGiphy and searchGiphy" key="2">
-            <Row align='center'>
-              {/* SEARCH BOX  */}
-              <Col span={8} />
-              <Col span={8}>
-                <Search
-                  placeholder="Search SOAPBOX Giphys"
-                  enterButton="Search"
-                  size="large"
-                  suffix={suffix}
-                  onSearch={onSearch}
-                  onChange={(e) => setQuery(e.target.value)}
-                  allowClear
-                />
-                <Space />
-              </Col>
-              <Col span={8} />
-            </Row>
-            <Row align='center'>
-              {query.length === 0
-                ?
-                <><br /><br />
-                <Carousel
-                  fetchGifs={fetchGifs}
-                  gutter={8}
-                  gifHeight={150}
-                  hideAttribution={true}
-                  borderRadius={5}
-                />ƒ
-                </> 
-                : 
-                <><br /><br />
-                <Grid
-                key={query}
-                gutter={8}
-                width={800}
-                columns={3}
-                fetchGifs={() => searchGifs(5)}
-                borderRadius={5}
-                noResultsMessage={<Result
-                  status="404"
-                  title="No Results found"
-                  subTitle="Sorry, No results found for the searched keyword"
-                />}
-              />
-              </>
-              }
-            </Row>
+            <TabPane tab="Using fetchGiphy and searchGiphy" key="2">
+              <Row align='center'>
+                {/* SEARCH BOX  */}
+                <Col span={8} />
+                <Col span={8}>
+                  <Search
+                    placeholder="Search SOAPBOX Giphys"
+                    enterButton="Search"
+                    size="large"
+                    suffix={suffix}
+                    onSearch={onSearch}
+                    onChange={(e) => setQuery(e.target.value)}
+                    allowClear
+                  />
+                  <Space />
+                </Col>
+                <Col span={8} />
+              </Row>
+              <Row align='center'>
+                {query.length === 0
+                  ?
+                  <><br /><br />
+                    <Carousel
+                      fetchGifs={fetchGifs}
+                      gutter={8}
+                      gifHeight={150}
+                      hideAttribution={true}
+                      borderRadius={5}
+                    />
+                  </>
+                  :
+                  <><br /><br />
+                    <Grid
+                      key={query}
+                      gutter={8}
+                      width={800}
+                      columns={3}
+                      fetchGifs={() => searchGifs(5)}
+                      borderRadius={5}
+                      noResultsMessage={<Result
+                        status="404"
+                        title="No Results found"
+                        subTitle="Sorry, No results found for the searched keyword"
+                      />}
+                    />
+                  </>
+                }
+              </Row>
+            </TabPane>
 
-
-
-          </TabPane>
-          
-        </Tabs>
+          </Tabs>
         </Col>
       </Row>
 
-
-
-
-
-
-    
+      <div class="footer">
+        <p><a href='https://linktr.ee/ianasqazi'>Anas Qazi</a></p>
+      </div>
 
     </div>
   );
--- src/components/ResultList.js
+++ src/components/ResultList.js
@@ -5,30 +5,30 @@ const { Meta } = Card;
 
 function ResultList(props) {
   return (<><div class='site-card-wrapper'><Row gutter={40} align={'center'}>
-      {props.results.map((result) => (
-           <Col><Card
-          key={result.id}
-          title={result.title}
-          hoverable
-          style={{ width: 240, height: 350, margin: 10 }}
-          cover={<a href={result.url}>
-          <img 
-          alt={result.title} 
-          src={result.images.original.url}
-          width={200}
-          height={180}
+    {props.results.map((result) => (
+      <Col><Card
+        key={result.id}
+        title={result.title}
+        hoverable
+        style={{ width: 240, height: 350, margin: 10 }}
+        cover={<a href={result.url}>
+          <img
+            alt={result.title}
+            src={result.images.original.url}
+            width={200}
+            height={180}
           />
-          </a>
-          }
-          >
-          <Meta 
-            description={result.source}
-          />
-        </Card>
-        </Col>
-      ))}
-      </Row>
-      </div>
+        </a>
+        }
+      >
+        <Meta
+          description={result.source}
+        />
+      </Card>
+      </Col>
+    ))}
+  </Row>
+  </div>
   </>
   );
 }
